66d9b0942845ce6aff784f129ba1f65d8b1b3d59
Horoli <sunha321@gmail.com>
2024-08-11 01:00:30 +0900

    wallpaper update

--- lib/preset/image.dart
+++ lib/preset/image.dart
@@ -1,3 +1,3 @@
-const String APPLICTION_WALLPAPER = 'assets/images/324wallpaper.jpeg';
+const String APPLICTION_WALLPAPER = 'assets/images/325wallpaper.jpeg';
 
 const String TEST = 'assets/images/mirror_house.png';
