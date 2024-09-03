eb38f1fc30151aae39a23f31a9b60c34336dfc13
Horoli <sunha321@gmail.com>
2024-09-03 18:02:49 +0900

    update

--- lib/adfit/interface.dart
+++ lib/adfit/interface.dart
@@ -8,7 +8,7 @@ abstract class KakaoAdfit {
 
   final String banner = """
   <ins class="kakao_ad_area" style="display:none;"
-data-ad-unit = "DAN-wWXnZLGfKToohs0h"
+data-ad-unit = "DAN-ovwGNveql4ZmwblT"
 data-ad-width = "160"
 data-ad-height = "600"></ins>
 <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
